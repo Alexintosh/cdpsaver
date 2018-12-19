e271afdde0b058cbc92b22cdd4d3b4c5eeaafc51
astankovic <aleksa.stankovic@berkeley.edu>
2018-12-19 12:13:53 +0100

    Add common scss files, add App header, fix routing.

--- client/src/components/App/Routes.jsx
+++ client/src/components/App/Routes.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect, Provider } from 'react-redux';
 import PropTypes from 'prop-types';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../Header/Header';
 import Homepage from '../HomePage/Homepage';
 import MarketplacePage from '../MarketplacePage/MarketplacePage';
 import SaverPage from '../SaverPage/SaverPage';
@@ -17,13 +18,17 @@ class RoutesWrapper extends Component {
     return (
       <Provider store={store}>
         <BrowserRouter>
-          <Switch>
-            <Route path="/" component={Homepage} />
-            <Route path="/marketplace" component={MarketplacePage} />
-            <Route path="/saver" component={SaverPage} />
-            <Route path="/notifier" component={NotifierPage} />
-            <Route component={Page404} />
-          </Switch>
+          <div>
+            <Header />
+
+            <Switch>
+              <Route exact path="/" component={Homepage} />
+              <Route path="/marketplace" component={MarketplacePage} />
+              <Route path="/saver" component={SaverPage} />
+              <Route path="/notifier" component={NotifierPage} />
+              <Route path="*" component={Page404} />
+            </Switch>
+          </div>
         </BrowserRouter>
       </Provider>
     );
