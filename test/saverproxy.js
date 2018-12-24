c53687301097db1110aa0c9192e2dd1c6e0d50c0
Nenad Palinkasevic <nesa993@gmail.com>
2018-12-24 09:12:20 +0100

    Setting up the kovan testing enviroment and getting stats about liquidity

---
+++ test/saverproxy.js
@@ -0,0 +1,7 @@
+const SaverProxy = artifacts.require("./SaverProxy.sol");
+
+contract("SaverProxy", accounts => {
+  it("...get the deployed contract", async () => {
+    console.log(accounts);
+  });
+});
--- truffle.js
+++ truffle.js
@@ -1,7 +1,31 @@
 const path = require("path");
 
+const dotenv           = require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const mnemonic = process.env.ETHEREUM_ACCOUNT_MNEMONIC;
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, `https://kovan.infura.io/`);
+      },
+      network_id: '42',
+      },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/');
+      },
+      network_id: '4',
+    },
+    test: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'http://127.0.0.1:8545/');
+      },
+      network_id: '*',
+    },
+  },
   contracts_build_directory: path.join(__dirname, "client/src/contracts")
-};
+
+}
