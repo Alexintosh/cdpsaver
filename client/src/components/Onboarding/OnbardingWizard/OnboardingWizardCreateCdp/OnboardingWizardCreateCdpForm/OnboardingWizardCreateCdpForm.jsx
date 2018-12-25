070e7878a67763820d3dd4906ccdbcfd8ec7618a
astankovic <aleksa.stankovic@berkeley.edu>
2018-12-25 11:42:09 +0100

    Add rainbow slider to the onboaring wizard create cdp form.

--- client/src/components/Onboarding/OnbardingWizard/OnboardingWizardCreateCdp/OnboardingWizardCreateCdpForm/OnboardingWizardCreateCdpForm.jsx
+++ client/src/components/Onboarding/OnbardingWizard/OnboardingWizardCreateCdp/OnboardingWizardCreateCdpForm/OnboardingWizardCreateCdpForm.jsx
@@ -1,43 +1,98 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
+import Slider from 'rc-slider';
 import onboardingWizardCreateCdpFormValidator from './onboardingWizardCreateCdpFormValidator';
 import InputComponent from '../../../../Forms/InputComponent';
 import { createCdpAction } from '../../../../../actions/onboardingActions';
+import { getRainbowSliderValColor } from '../../../../../services/utils';
 
-let OnboardingWizardCreateCdpForm = ({ handleSubmit, onSubmit, history }) => (
-  <form
-    id="onboarding-wizard-create-cdp-form"
-    onSubmit={handleSubmit((e) => { onSubmit(e, history); })}
-    className="form-wrapper"
-  >
-    <Field
-      id="onboarding-create-cdp-eth-amount"
-      name="ethAmount"
-      placeholder="1"
-      type="number"
-      labelText="Amount of ETH:"
-      secondLabelText="ETH"
-      additional={{ min: 1 }}
-      component={InputComponent}
-      showErrorText
-      focus
-    />
+import '../../../../../common/slider.scss';
 
-    <Field
-      id="onboarding-create-cdp-dai-amount"
-      name="daiAmount"
-      placeholder="1"
-      type="number"
-      labelText="Amount of DAI:"
-      secondLabelText="DAI"
-      additional={{ min: 1 }}
-      component={InputComponent}
-      showErrorText
-    />
-  </form>
-);
+class OnboardingWizardCreateCdpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentColor: '',
+    };
+
+    this.sliderWidth = 284;
+    this.gradients = [
+      [0, [255, 0, 0]],
+      [49, [255, 141, 0]],
+      [100, [72, 189, 0]],
+    ];
+
+    this.handleSliderValChange = this.handleSliderValChange.bind(this);
+  }
+
+  componentDidMount() {
+    // CHANGE THIS IF THERE WILL BE A DIFFERENT DEFAULT SLIDER VALUE
+    this.handleSliderValChange(1);
+  }
+
+  handleSliderValChange(val) {
+    const rbgArray = getRainbowSliderValColor(val, this.gradients, this.sliderWidth);
+    this.setState({ currentColor: `rgb(${rbgArray.join(',')})` });
+  }
+
+  render() {
+    const { handleSubmit, onSubmit, history } = this.props;
+
+    return (
+      <form
+        id="onboarding-wizard-create-cdp-form"
+        onSubmit={handleSubmit((e) => { onSubmit(e, history); })}
+        className="form-wrapper"
+      >
+        <Field
+          id="onboarding-create-cdp-eth-amount"
+          name="ethAmount"
+          placeholder="1"
+          type="number"
+          labelText="Amount of ETH:"
+          secondLabelText="ETH"
+          additional={{ min: 1 }}
+          component={InputComponent}
+          showErrorText
+          focus
+        />
+
+        <Field
+          id="onboarding-create-cdp-dai-amount"
+          name="daiAmount"
+          placeholder="1"
+          type="number"
+          labelText="Amount of DAI:"
+          secondLabelText="DAI"
+          additional={{ min: 1 }}
+          component={InputComponent}
+          showErrorText
+        />
+        <div className="slider-wrapper">
+          <Slider
+            ref={this.slider}
+            onChange={this.handleSliderValChange}
+            handleStyle={{ backgroundColor: this.state.currentColor }}
+            min={1}
+            max={100}
+          />
+
+          <div className="liquidation">
+            <div className="label">Liquidation price:</div>
+            <div
+              style={{ color: this.state.currentColor }}
+              className="value"
+            >
+              40%
+            </div>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
 
 OnboardingWizardCreateCdpForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
@@ -45,7 +100,7 @@ OnboardingWizardCreateCdpForm.propTypes = {
   history: PropTypes.object.isRequired,
 };
 
-OnboardingWizardCreateCdpForm = reduxForm({
+const OnboardingWizardCreateCdpFormComp = reduxForm({
   form: 'onboardingWizardCreateCdpForm',
   validate: onboardingWizardCreateCdpFormValidator,
 })(OnboardingWizardCreateCdpForm);
@@ -56,4 +111,4 @@ const mapDispatchToProps = {
   onSubmit: createCdpAction,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(OnboardingWizardCreateCdpForm);
+export default connect(mapStateToProps, mapDispatchToProps)(OnboardingWizardCreateCdpFormComp);
---
+++ client/src/services/utils.js
@@ -0,0 +1,47 @@
+/**
+ * Takes two rgb arrays and gradient weight and calculates combined rgb color
+ *
+ * @param color1 {Array}
+ * @param color2 {Array}
+ * @param weight {Number}
+ *
+ * @return {*[]}
+ */
+export const pickHex = (color1, color2, weight) => {
+  const w = (weight * 2) - 1;
+  const w1 = (w + 1) / 2;
+  const w2 = 1 - w1;
+  return [Math.round(color1[0] * w1 + color2[0] * w2), Math.round(color1[1] * w1 + color2[1] * w2), Math.round(color1[2] * w1 + color2[2] * w2)]; // eslint-disable-line
+};
+
+/**
+ * Calculates and returns hex color for handle point on the rainbow slider
+ *
+ * @param val {Number}
+ * @param gradients {Array}
+ * @param sliderWidth {Number}
+ *
+ * @return {Array}
+ */
+export const getRainbowSliderValColor = (val, gradients, sliderWidth) => {
+  let colorRange = [];
+
+  gradients.forEach((value, index) => {
+    if (val <= value[0] && (colorRange.length === 0)) {
+      colorRange = [index - 1, index];
+    }
+  });
+
+  // Get the two closest colors
+  const firstColor = gradients[colorRange[0]][1];
+  const secondColor = gradients[colorRange[1]][1];
+
+  // Calculate ratio between the two closest colors
+  const firstColorX = sliderWidth * (gradients[colorRange[0]][0] / 100);
+  const secondColorX = (sliderWidth * (gradients[colorRange[1]][0] / 100)) - firstColorX;
+  const sliderX = (sliderWidth * (val / 100)) - firstColorX;
+  const ratio = sliderX / secondColorX;
+
+  // Get the color with pickHex(thx, less.js's mix function!)
+  return pickHex(secondColor, firstColor, ratio);
+};
