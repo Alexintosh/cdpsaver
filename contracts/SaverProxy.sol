4c8c1f36eefa699d63641cc5b52858ce809eed5f
Nenad Palinkasevic <nesa993@gmail.com>
2019-01-08 14:33:08 +0100

    Calculating max available collateral to free

--- contracts/SaverProxy.sol
+++ contracts/SaverProxy.sol
@@ -5,22 +5,34 @@ import "./interfaces/ERC20.sol";
 import "./interfaces/KyberNetworkProxyInterface.sol";
 import "./SaiProxy.sol";
 
+contract IVox {
+    function par() public returns (uint); //ref per dai
+}
+
 contract SaverProxy is SaiProxy {
-    function unwind(address _tub, uint _cdpId) public {
+
+    function repay(address _tub, address _vox, uint _cdpId) public {
+        TubInterface tub = TubInterface(_tub);
+        bytes32 cup = bytes32(_cdpId);
+
+        uint maxCollateral = maxFreeCollateral(tub, _vox, bytes32(_cdpId));
+
+        free(_tub, cup, maxCollateral);
+
+        //convert eth -> dai
+        //swapEtherToToken();
+
+        //wipe() debt
+
+        
+    }
+
+    function boost(address _tub, uint _cdpId) public {
         TubInterface cdp = TubInterface(_tub);
-
-        uint maxCollateral = getMaxFreeCollateral();
-
-        cdp.free(bytes32(_cdpId), maxCollateral);
-
-        uint daiAmount = swapEtherToToken();
-
-        //User needs maker
-        cdp.wipe(bytes32(_cdpId), daiAmount);
     }
 
     //TODO: watch out for gas price limits
-    function swapEtherToToken () internal returns(uint) {
+    function swapEtherToToken (uint _ethAmount) internal returns(uint) {
 
         uint minRate;
         ERC20 ETH_TOKEN_ADDRESS = ERC20(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
@@ -28,17 +40,17 @@ contract SaverProxy is SaiProxy {
 
         KyberNetworkProxyInterface _kyberNetworkProxy = KyberNetworkProxyInterface(0x7e6b8b9510D71BF8EF0f893902EbB9C865eEF4Df); //KOVAN ADDRESS
 
-        (, minRate) = _kyberNetworkProxy.getExpectedRate(ETH_TOKEN_ADDRESS, daiToken, msg.value);
+        (, minRate) = _kyberNetworkProxy.getExpectedRate(ETH_TOKEN_ADDRESS, daiToken, _ethAmount);
 
         //will send back tokens to this contract's address
-        uint destAmount = _kyberNetworkProxy.swapEtherToToken.value(msg.value)(daiToken, minRate);
+        uint destAmount = _kyberNetworkProxy.swapEtherToToken.value(_ethAmount)(daiToken, minRate);
 
         return destAmount;
     }
 
-    //TODO: calculate what is the max amount of collateral you can get
-    function getMaxFreeCollateral() pure internal returns(uint) {
-        return 10000;
+    function maxFreeCollateral(TubInterface _tub, address _vox, bytes32 _cdpID) public returns (uint) {
+        return sub(_tub.ink(_cdpID), wdiv(wmul(wmul(_tub.tab(_cdpID), _tub.mat()), IVox(_vox).par()), _tub.tag()));
     }
+
 
 }
