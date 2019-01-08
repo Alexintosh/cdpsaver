471a5464ba6840f939fd24e442543d7ca8eb9622
astankovic <aleksa.stankovic@berkeley.edu>
2019-01-08 13:09:31 +0100

    Remove excess wrappers.

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect, Provider } from 'react-redux';
 import PropTypes from 'prop-types';
-import Routes from './Routes';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../Header/Header';
+import HomePage from '../HomePage/HomePage';
+import OnboardingRoutes from '../Onboarding/OnboardingRoutes';
+import Page404 from '../Page404/Page404';
+import DashboardRoutes from '../Dashboard/DashboardRoutes';
+import ModalRoot from '../Modals/ModalRoot';
 
 import './App.scss';
+import '../../common/icons/icons.scss';
 
-const App = ({ store }) => (
-  <div className="app-wrapper">
-    <Routes store={store} />
-  </div>
-);
+class RoutesWrapper extends Component {
+  componentWillMount() {}
 
-App.propTypes = {
+  render() {
+    const { store } = this.props;
+
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="app">
+            <Header />
+
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/onboarding" component={OnboardingRoutes} />
+              <Route path="/dashboard" component={DashboardRoutes} />
+              <Route path="*" component={Page404} />
+            </Switch>
+
+            <ModalRoot />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
+
+RoutesWrapper.propTypes = {
   store: PropTypes.object.isRequired,
 };
 
-export default App;
+const mapDispatchToProps = {};
+
+export default connect(null, mapDispatchToProps)(RoutesWrapper);
