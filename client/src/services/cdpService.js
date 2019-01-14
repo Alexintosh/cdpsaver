7b3759ec28e44991ef1f04e9a899ffa62e20d88d
astankovic <aleksa.stankovic@berkeley.edu>
2019-01-14 18:09:01 +0100

    Get cdp id using proxy address partial.

--- client/src/services/cdpService.js
+++ client/src/services/cdpService.js
@@ -1,7 +1,8 @@
 import Maker from '@makerdao/dai';
 import web3 from 'web3';
 import clientConfig from '../config/clientConfig.json';
-import { proxyRegistryInterfaceContract } from './contractRegistryService';
+import { proxyRegistryInterfaceContract, SaiTubAddressContract } from './contractRegistryService';
+import { MOCK_CDP } from '../constants/general';
 
 const maker = Maker.create('http', { url: clientConfig.providerUrl });
 
@@ -71,17 +72,65 @@ export const createCdp = async (ethAmount, daiAmount) => {
 };
 
 /**
+ * Calls the SaiTub contract and fetches cdp for proxy address
+ *
+ * @param sig
+ * @param bar
+ *
+ * @return {Promise<any>}
+ */
+export const getCdpForAddress = (sig, bar) => new Promise(async (resolve, reject) => {
+  try {
+    const contract = await SaiTubAddressContract();
+
+    const event = await contract.getPastEvents('LogNote', {
+      filter: { bar, sig },
+      fromBlock: 5216602,
+    });
+
+    console.log('event', event);
+  } catch (e) {
+    console.log('ERR', e);
+    reject(e);
+  }
+});
+
+const x = {
+  u: (e, t, n) => new Array(t - e.length + 1).join(n || "0") + e, // eslint-disable-line
+  m: e => web3.utils.sha3(e).substring(0, 10),
+  q: (e) => {
+    const t = !(arguments.length > 1 && void 0 !== arguments[1]) || arguments[1]; // eslint-disable-line
+    let n = web3.utils.toHex(e);
+
+    return n = n.replace('0x', ''), // eslint-disable-line
+    n = x.u(n, 64), // eslint-disable-line
+    t && (n = '0x' + n), // eslint-disable-line
+    n; // eslint-disable-line
+  },
+};
+
+/**
  * Checks if the connected user has a cdp for their address
  *
  */
 export const getAddressCdp = address => new Promise(async (resolve, reject) => {
   if (!address) return reject('user has no address');
 
+  const sig = Object(x.m)('give(bytes32,address)');
+  const bar = Object(x.q)(address);
   try {
-    const result = await proxyRegistryInterfaceContract().methods.proxies(address).call();
+    // const proxyAddress = await proxyRegistryInterfaceContract().methods.proxies(address).call();
+    // console.log('proxyAddress', proxyAddress);
+    // const checksumAddress = web3.utils.toChecksumAddress(proxyAddress);
 
-    resolve(result);
+    const cdpMeta = await getCdpForAddress(sig, bar);
+    console.log('cdpMeta', cdpMeta);
+    return;
+    const cdp = await getCdpInfo(cdpMeta.id, true);
+
+    resolve(MOCK_CDP);
   } catch (err) {
+    console.log('ERROR', err);
     reject(err);
   }
 });
