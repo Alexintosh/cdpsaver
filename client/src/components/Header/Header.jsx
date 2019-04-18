04e5a4ff1aa70c30843a834d4464beeb90db45d2
astankovic <aleksa.stankovic@berkeley.edu>
2019-04-18 10:07:29 +0200

    Add new logo to the header.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 import './Header.scss';
+import logo from './logo.svg';
 
 const Header = () => (
   <div className="header-wrapper">
     <div className="width-container">
-      <Link className="logo-wrapper" to="/">
-        <span>CDP</span>
-        Saver
-      </Link>
+      <Link className="logo-wrapper" to="/"><img src={logo} alt="logo" /></Link>
 
       <div className="links-wrapper">
         <NavLink activeClassName="active" to="/connect">Connect wallet</NavLink>
